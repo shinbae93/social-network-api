dffdc20452ffb1b4d213c9eb713cceb40caf9668
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2022-11-16 00:17:02 +0700

    [service/router] add functions for authen/author and add some feature: login/register/logout

--- src/services/UserManager.js
+++ src/services/UserManager.js
@@ -15,6 +15,44 @@ UserManager.prototype.findUsers = async function(criteria, more) {
   return output;
 };
 
-/* Other services here */
+UserManager.prototype.getUser = async function(userId, more) {
+  const user = await User.findById(userId);
+  //
+  if (!user) {
+    throw new Error(`Not found user with id [${userId}]!`);
+  }
+  //
+  return user;
+};
+
+UserManager.prototype.createUser = async function(userObj, more) {
+  const user = new User(userObj);
+  const output = {};
+  //
+  if (more && more.generateAuthToken === true) {
+    const token = await this.generateAuthToken(user._id);
+    user.refreshTokens = user.refreshTokens.concat({ token });
+    //
+    output.token = token;
+  };
+  //
+  await user.save();
+  output.user = user;
+  //
+  return output;
+};
+
+UserManager.prototype.generateAuthToken = async function(userId, more) {
+  const user = await this.getUser(userId);
+  const AUTH_KEY = 'social network';
+  //
+  const token = jwt.sign({
+    _id: user._id,
+    name: user.name,
+    email: user.email
+  }, AUTH_KEY, {expiresIn: 864000});
+  //
+  return token;
+};
 //
 module.exports = { UserManager };
