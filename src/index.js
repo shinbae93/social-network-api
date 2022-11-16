5d8179c228bb3bfdd2d6888cc2c832b9cde37308
Hung Nguyen <hungquoc.nguyen@team.enouvo.com>
2022-11-16 11:22:51 +0700

    setup sentry

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const Sentry = require('@sentry/node');
 require('./models/database/mongoose');
 const userRouter = require('./routers/web-user-manager');
 const postRouter = require('./routers/web-post-manager');
-//
+
 const app = express();
+
+/** Middle wares */
+// Init sentry
+Sentry.init({ dsn: 'https://3438260379c547c0a958a7a3335bccd5@o4504166714834944.ingest.sentry.io/4504166720536576' });
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+// Add other middleware bellow
 app.use(cors());
-const PORT = process.env.PORT || 3000;
-//
 app.use(express.json());
-//
+
+/** Routers */
 app.use(userRouter);
 app.use(postRouter);
-//
+app.get('/debug-sentry', function mainHandler(req, res) {
+  throw new Error('My first Sentry error!');
+});
+
+// The sentry error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+// Add other error middleware bellow
+
+/** Start server */
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
 });
