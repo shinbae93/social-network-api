53b7c358b62238c1ea0601cd819694c6086ad264
Nguyễn Văn Quốc Hùng <76779880+shinbae93@users.noreply.github.com>
2022-11-17 11:14:55 +0700

    Revert "Feature/api comment"

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-## Init
-```
-npm install
-```
-## Database connection
-- Go to models/database/mongoose.js to config Connection with mongodb
 ## Run server
 ```
 npm run server
--- src/models/database/mongoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const CONNECTION_URL = 'mongodb://localhost:27017/social-network';
-mongoose.connect(CONNECTION_URL, {
-  useNewUrlParser: true
-});
