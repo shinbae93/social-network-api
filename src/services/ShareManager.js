a4c80d8beaa5f517f666aa25945bd5894dc93018
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2022-11-22 00:54:44 +0700

    [service/router][Post-Share] intergrate Post with Share

--- src/services/ShareManager.js
+++ src/services/ShareManager.js
@@ -7,14 +7,14 @@ function ShareManager(params) {}
 //
 ShareManager.prototype.createShare = async function (shareObj, more) {
   const share = new Share(shareObj);
+  //
   const post = await Post.findById(shareObj.postId);
-  const newPost = await Post.findByIdAndUpdate(
-    shareObj.postId,
-    {
-      totalShares: post.totalShares + 1
-    },
-    { new: true }
-  );
+  if (!post) {
+    throw new Error(`Not found post with id [${shareObj.postId}]`);
+  }
+  await Post.findByIdAndUpdate(shareObj.postId, {
+    totalShares: post.totalShares + 1
+  }, { new: true });
   const output = {};
   //
   await share.save();
@@ -23,17 +23,16 @@ ShareManager.prototype.createShare = async function (shareObj, more) {
   return output;
 };
 
-ShareManager.prototype.deleteShare = async function (ShareId, more) {
-  const share = await Share.findOneAndDelete(ShareId);
+ShareManager.prototype.deleteShare = async function (shareId, more) {
+  const share = await Share.findOneAndDelete(shareId);
+  //
   const post = await Post.findById(share.postId);
-  console.log(post);
-  const newPost = await Post.findByIdAndUpdate(
-    share.postId,
-    {
-      totalShares: post.totalShares - 1
-    },
-    { new: true }
-  );
+  if (!post) {
+    throw new Error(`Not found post with id [${share.postId}]`);
+  }
+  await Post.findByIdAndUpdate(share.postId, {
+    totalShares: post.totalShares - 1
+  }, { new: true });
   const output = {};
   //
   output.share = share;
