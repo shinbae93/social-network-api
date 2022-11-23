33696af7f1dd1ac8305dc3aa60876cd3de5c3da8
hvtruong.2209 <hvtruong.2209@gmail.com>
2022-11-23 13:05:41 +0700

    add images into post

--- src/services/PostManager.js
+++ src/services/PostManager.js
@@ -2,10 +2,11 @@ const lodash = require('lodash');
 const { User } = require('../models/_User');
 const { Post } = require('../models/_Post');
 const { CommentManager } = require('./CommentManager');
+var cloudinary = require('cloudinary').v2;
 //
-function PostManager(params) {};
+function PostManager(params) {}
 //
-PostManager.prototype.createPost = async function(postObj, more) {
+PostManager.prototype.createPost = async function (postObj, more) {
   const post = new Post(postObj);
   const output = {};
   //
@@ -15,17 +16,43 @@ PostManager.prototype.createPost = async function(postObj, more) {
   return output;
 };
 //
-PostManager.prototype.findPosts = async function(criteria, more) {
-  let posts = await Post.find().sort([['createdAt', -1]]).populate("comments");
+PostManager.prototype.updatePost = async function (id, postObj, more) {
+  const post = await Post.findByIdAndUpdate({ _id: id }, postObj, {
+    new: true
+  });
+  const output = {};
+  //
+  output.post = post;
+  //
+  return output;
+};
+//
+PostManager.prototype.findPosts = async function (criteria, more) {
+  let posts = await Post.find()
+    .sort([['createdAt', -1]])
+    .populate('comments');
   //
   const output = {
     rows: posts,
     count: posts.length
-  }
+  };
   //
   return output;
 };
-
+//
+PostManager.prototype.removeImage = async function (id, more) {
+  let posts = await Post.findById(id);
+  //
+  if (posts.attachments.length != 0) {
+    JSON.parse(posts.attachments).map(el => {
+      const string = 'posts/' + el.split('/').pop().split('.')[0];
+      cloudinary.uploader.destroy(string, function (error, result) {});
+    });
+    //
+  }
+  //
+};
+//
 /* Other services here */
 //
 module.exports = { PostManager };
