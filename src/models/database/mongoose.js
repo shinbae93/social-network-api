651a546242a7bd085d0b28d07bbbcd8762d116f5
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2022-12-03 23:18:05 +0700

    [mongo] change to remote mongo

--- src/models/database/mongoose.js
+++ src/models/database/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 // const CONNECTION_URL = 'mongodb://localhost:27017/social-network';
-const CONNECTION_URL = 'mongodb://172.17.0.2:27017/social-network';
+const CONNECTION_URL = 'mongodb://tcp-mo4.mogenius.io:37532';
 mongoose.connect(CONNECTION_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  user: "root",
+  pass: "123456",
+  dbName: "social-network"
 });
