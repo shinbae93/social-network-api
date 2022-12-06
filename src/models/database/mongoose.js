3e0b6cf4083efeaec4231f7064f27a73a0ff1fab
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2022-12-06 14:42:50 +0700

    [config] change port of mongo

--- src/models/database/mongoose.js
+++ src/models/database/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
-// const CONNECTION_URL = 'mongodb://localhost:27017/social-network';
-const CONNECTION_URL = 'mongodb://tcp-mo4.mogenius.io:37532';
+// const CONNECTION_URL = 'mongodb://172.17.0.2:27017/social-network';
+// const CONNECTION_URL = 'mongodb://172.17.0.2:27017/social-network-test';
+// mongoose.connect(CONNECTION_URL, {
+//   useNewUrlParser: true,
+// });
+const CONNECTION_URL = 'mongodb://tcp-mo4.mogenius.io:41050';
 mongoose.connect(CONNECTION_URL, {
   useNewUrlParser: true,
   user: "root",
