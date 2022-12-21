75e6495232a1e44e5ada7057b6fbb0db8f328ba7
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2022-12-21 09:31:37 +0700

    [config] change mongo's port

--- src/models/database/mongoose.js
+++ src/models/database/mongoose.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
-const CONNECTION_URL = 'mongodb://tcp-mo4.mogenius.io:60390';
+const CONNECTION_URL = 'mongodb://tcp-mo4.mogenius.io:13947';
+
 mongoose.connect(CONNECTION_URL, {
   useNewUrlParser: true,
   user: "root",
   pass: "123456",
   dbName: "social-network"
+}, function (error) {
+  if (error) {
+    throw error;
+  }
+  console.log('Connected to database');
 });
